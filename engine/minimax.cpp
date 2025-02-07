99668fbcad70dfa43482f78d4e8e3a6a9efc1d47
Matthew-Kirkland <matthewkirkland049@gmail.com>
2025-02-07 17:27:39 -0500

    begin migrating to C++

---
+++ engine/minimax.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <stdint.h>
--- include/game.h
+++ include/game.h
@@ -1,6 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <unistd.h>
+
 #define NUM_ROWS 6
 #define NUM_COLS 7
 #define NUM_TO_WIN 4
@@ -14,27 +19,29 @@
 #define FIRST_TOKEN 'X'
 #define SECOND_TOKEN 'O'
 
-typedef struct game *Game;
-
-struct game {
-    uint64_t p1Board;
-    uint64_t p2Board;
-    int moveStack[MAX_MOVES];
-    int moveCount;
-    bool turn;
-};
-
 extern char tokens[];
 
-Game initialiseGame();
-void printBoard(Game game);
-bool hasWonHorizontal(Game game, bool turn);
-bool hasWonVertical(Game game, bool turn);
-bool hasWonDiagonalTLBR(Game game, bool turn);
-bool hasWonDiagonalBLTR(Game game, bool turn);
-bool hasWon(Game game, bool turn);
-void placeTile(Game game, int playerInput, int insertRow);
-void gameLoop(Game game);
-void undoMove(Game game);
+class Game {
+    private:
+        uint64_t p1Board;
+        uint64_t p2Board;
+        int moveStack[MAX_MOVES];
+        int moveCount;
+        bool turn;
+    
+    public:
+        Game();
+
+        void clearScreen() const;
+        void printBoard() const;
+        bool hasWonHorizontal(bool who) const;
+        bool hasWonVertical(bool who) const;
+        bool hasWonDiagonalTLBR(bool who) const;
+        bool hasWonDiagonalBLTR(bool who) const;
+        bool hasWon(bool who) const;
+        void placeTile(int playerInput, int insertRow);
+        void gameLoop();
+        void undoMove();
+    };
 
 #endif
