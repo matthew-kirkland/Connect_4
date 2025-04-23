c5dcd5ef4ab9d191ed230dc13fd88c58518cd1c6
Matthew-Kirkland <matthewkirkland049@gmail.com>
2025-04-23 13:54:59 -0400

    added filled board check to termination condition

--- engine/minimax.cpp
+++ engine/minimax.cpp
@@ -3,14 +3,14 @@
 #include "../include/minimax.h"
 
 Move minimax(Game game, int depth, bool originalPlayer) {
-    if (depth == 0 || game.hasWon(game.turn) || game.hasWon(!game.turn)) {
+    if (depth == 0 || game.hasWon(game.turn) || game.hasWon(!game.turn) || game.moveCount >= MAX_MOVES) {
         return Move(evaluate(game, originalPlayer), -1);
     }
     bool isMax = (game.turn == originalPlayer);
     Move bestMove(isMax ? INT_MIN : INT_MAX, -1);
     for (int i = 0; i < NUM_COLS; i++) {
         if (game.columnFull(i)) continue;
-        
+
         game.placeTile(i, NUM_ROWS - 1);
         Move childMove = minimax(game, depth - 1, originalPlayer);
         if ((isMax && childMove.value > bestMove.value) || (!isMax && childMove.value < bestMove.value)) {
